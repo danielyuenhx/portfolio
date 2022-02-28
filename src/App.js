432ba7b66408e80e9a41d5475cdda868c0cc1d74
danielyuenhx <yuendaniel33@gmail.com>
2022-02-28 15:47:42 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+        <a class="logo" href='#'>Logo</a>
+        <ul>
+            <li><a id="link-1" href='#div-1' class="active">Home</a></li>
+            <li><a id="link-2" href='#div-2'>Introduction</a></li>
+            <li><a id="link-3" href='#div-3'>Products</a></li>
+        </ul>
+    </header>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/App.js",
+    "./src/index.js",
+    "./src/App.text.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
