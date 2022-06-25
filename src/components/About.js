0791707fc1bed062b3294507f5482c98d4d911fa
danielyuenhx <yuendaniel33@gmail.com>
2022-06-25 17:20:59 +0800

    reworked about

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react'
-import SmoothCollapse from 'react-smooth-collapse'
+import { motion } from "framer-motion"
+import SkillsIcon from './SkillsIcon';
 
 function About() {
     const [expanded, setExpanded] = useState();
 
     return (
         <div className='h-screen w-screen flex justify-center items-center font-mono text-left'>
-            <div className='flex w-9/12 items-center justify-around'>
+            <div className='flex w-8/12 items-center justify-around py-10 rounded-3xl border-2 drop-shadow-md'>
                 {/* <div className='w-5/12'>
                     <h1 className='text-5xl font-title py-6 tracking-tight align-top'>Skills</h1>
                     Programming Languages
@@ -21,8 +22,11 @@ function About() {
                     SQL/MySQL, Tableau
                 </div> */}
                 <div className='w-5/12'>
-                    <h1 className='text-5xl font-title py-6 tracking-tight align-top'>About me</h1>
+                    <h1 className='text-5xl font-title pb-6 tracking-tight align-top font-semibold'>About me</h1>
                     I'm a Final Year Software Engineering undergraduate at Monash University Malaysia who is enjoying all things related to programming. I mainly love working on web development because there is where creativity can shine ✨ You can check out some of my skills by hovering over the icons beside! 
+                </div>
+                <div className='w-5/12'>
+                    <SkillsIcon />
                 </div>
             </div>
         </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,16 @@ import linkedin from '../images/linkedin-white.png'
 function Footer() {
     return (
         <div className="flex flex-col justify-center items-center h-60 bg-[#282828] text-white font-header tracking-widest">
-            <p className='block'>PAGE BUILT BY DANIEL YUEN</p>
+            <p className='block'>© 2022 Daniel Yuen. All Rights Reserved.</p>
             <div>
                 <div className='inline-block'>
                     <a href="https://github.com/danielyuenhx" target="_blank">
-                        <img className='w-12 h-12 my-6 mr-4' src={github} />
+                        <img className='w-10 h-10 my-4 mr-4' src={github} />
                     </a>
                 </div>
                 <div className='inline-block'>
                     <a href="https://www.linkedin.com/in/danielyuenhx" target="_blank">
-                        <img className='w-12 h-12 my-6' src={linkedin} />
+                        <img className='w-10 h-10 my-4' src={linkedin} />
                     </a>
                 </div>
             </div>
